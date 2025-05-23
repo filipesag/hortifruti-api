510d5f7bada271b550851c0768e8d5a5c2fb4f66
Filipe Aguiar <filipesag@hotmail.com>
2025-05-23 16:47:47 -0300

    fix/corrigindo valores de taxas dos cartoes de acordo com DB

--- src/main/java/com/hortifruti/hortifrutiapi/model/enums/TipoPagamento.java
+++ src/main/java/com/hortifruti/hortifrutiapi/model/enums/TipoPagamento.java
@@ -3,17 +3,16 @@ package com.hortifruti.hortifrutiapi.model.enums;
 public enum TipoPagamento {
     DINHEIRO("Dinheiro", 0.0),
     PIX("Pix", 0.0),
-    CARTAO_DEBITO_GOLDMONEY("Cartão de Débito GoldMoney", 0.5),
-    CARTAO_DEBITO_VIDACARD("Cartão de Débito VidaCard", 0.5),
-    CARTAO_DEBITO_MASTERCOIN("Cartão de Débito MasterCoin", 0.5),
-    CARTAO_DEBITO_ALECARD("Cartão de Débito AleCard", 0.5),
-    CARTAO_CREDITO_GOLDMONEY("Cartão de Crédito GoldMoney", 1.5),
-    CARTAO_CREDITO_VIDACARD("Cartão de Crédito VidaCard", 1.5),
-    CARTAO_CREDITO_ALECARD("Cartão de Crédito AleCard", 1.5),
-    CARTAO_ALIMENTACAO_ALEXO("Cartão Alimentação Alexo", 0.0),
-    CARTAO_ALIMENTACAO_SODELO("Cartão Alimentação Sodelo", 0.0),
-    CARTAO_REFEICAO_ALEXO("Cartão Refeição Alexo", 0.0),
-    CARTAO_REFEICAO_SODELO("Cartão Refeição Sodelo", 0.0);
+    CARTAO_DEBITO_GOLDMONEY("Cartão de Débito GoldMoney", 2.22),
+    CARTAO_DEBITO_VIDACARD("Cartão de Débito VidaCard", 2.99),
+    CARTAO_DEBITO_MASTERCOIN("Cartão de Débito MasterCoin", 1.93),
+    CARTAO_CREDITO_MASTERCOIN("Cartão de Crédito MasterCoin",2.13),
+    CARTAO_CREDITO_GOLDMONEY("Cartão de Crédito GoldMoney", 2.33),
+    CARTAO_CREDITO_VIDACARD("Cartão de Crédito VidaCard", 3.02),
+    CARTAO_ALIMENTACAO_ALEXO("Cartão Alimentação Alexo", 1.77),
+    CARTAO_ALIMENTACAO_SODELO("Cartão Alimentação Sodelo", 2.21),
+    CARTAO_REFEICAO_ALEXO("Cartão Refeição Alexo", 1.99),
+    CARTAO_REFEICAO_SODELO("Cartão Refeição Sodelo", 3.50);
 
     private final String descricao;
     private final double taxa;
